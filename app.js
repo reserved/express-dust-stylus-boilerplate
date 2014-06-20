f866edc693279afe32c6b1d5f44434f76e1b0866
Ally Ent <logically.js@gmail.com>
2014-06-20 21:16:05 +0530

    updated app.js package.json

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
+var cons = require('consolidate');
+
+var stylus = require('stylus');
+var nib = require('nib');
+var jeet = require('jeet');
+var rupture = require('rupture');
+var typographic = require('typographic');
+var autoprefixerStylus = require('autoprefixer-stylus');
 
 var app = express();
 
 // view engine setup
+app.engine('dust',cons.dust);
+cons.dust.helpers = require('dustjs-helpers');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'dust');
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -22,7 +31,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,5 +63,19 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.use(stylus.middleware({
+  serve: true,
+  force: true,
+  src: __dirname + '/resources',
+  dest: __dirname + '/css',
+  compress: true,
+  compile: function(str, path) {
+   return stylus(str)
+     .set('filename', path)
+     .set('compress', true)
+     .use(nib(),jeet(),rupture(),typographic(),autoprefixerStylus())
+     .import('nib','jeet','rupture','typographic','autoprefixerStylus');
+ }
+}));
 
 module.exports = app;
